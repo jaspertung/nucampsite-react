5c2d9d68928b36ec69c926c55803afc6a347509a
Jasper Tung <jtunggsp@gmail.com>
2021-04-27 22:28:00 -0700

    Added Redux middleware to handle async calls

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,4 +1,5 @@
 import * as ActionTypes from './ActionTypes'
+import { CAMPSITES } from '../shared/campsites'
 
 export const addComment = (campsiteId, rating, author, text) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -8,4 +9,30 @@ export const addComment = (campsiteId, rating, author, text) => ({
         author: author,
         text:  text
     }
+})
+
+export const fetchCampsites = () => dispatch => {
+    dispatch(campsitesLoading())
+
+    setTimeout(() => {
+        dispatch(addCampsites(CAMPSITES))
+    }, 2000)
+}
+// uses thunk so can nest functions
+// simulating server request with setTimeout
+
+export const campsitesLoading = () => ({
+    type: ActionTypes.CAMPSITES_LOADING
+})
+// this action will be dispatched by fetchCampsites
+
+export const campsitesFailed = errMess => ({
+    type: ActionTypes.CAMPSITES_FAILED,
+    payload: errMess
+})
+// action that returns an error message
+
+export const addCampsites = campsites => ({
+    type: ActionTypes.ADD_CAMPSITES,
+    payload: campsites
 })
--- src/redux/campsites.js
+++ src/redux/campsites.js
@@ -1,7 +1,30 @@
-import { CAMPSITES } from '../shared/campsites'
+//import { CAMPSITES } from '../shared/campsites'
+// don't need anymore because receiving CAMPSITES data from an action
 
-export const Campsites = (state = CAMPSITES, action) => {
+import * as ActionTypes from './ActionTypes'
+// import action types instead
+
+/*export const Campsites = (state = CAMPSITES, action) => {
     switch (action.type) {
+        default:
+            return state
+    }
+
+change state to include 3 properties
+}*/
+
+export const Campsites = (state = {
+        isLoading: true,
+        errMess: null,
+        campsites: []
+    }, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_CAMPSITES:
+            return {...state, isLoading: false, errMess: null, campsites: action.payload}
+        case ActionTypes.CAMPSITES_LOADING:
+            return {...state, isLoading: true, errMess: null, campsites: []}
+        case ActionTypes.CAMPSITES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload}
         default:
             return state
     }
